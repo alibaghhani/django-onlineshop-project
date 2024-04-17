145841aad77409ae9484fe1aebb7ad28cb6c3d33
alibaghani <baghaniali2006@gmail.com>
2024-04-17 19:01:49 +0330

    product model was implemented

--- product/models.py
+++ product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from core.models import TimeStampMixin,LogicalDeleteMixin
 
 # Create your models here.
+class Product(TimeStampMixin,LogicalDeleteMixin):
+    name = models.CharField(max_length=250)
+    price = models.PositiveIntegerField()
+    title = models.TextField(max_length=250)
+    category = models.ForeignKey('Category',on_delete=models.PROTECT,related_name='product_category')
