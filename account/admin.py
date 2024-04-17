b3b2352fd132fdbb38d1552c45c99d7b2e45882e
alibaghani <baghaniali2006@gmail.com>
2024-04-17 19:26:47 +0330

    models were registered in admin panel

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Address
+from product.models import Image,Category,Product
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Address)
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Product)
