51af224e7215333a1513e8220df9fd81082af54c
alibaghani <baghaniali2006@gmail.com>
2024-04-17 19:04:21 +0330

    category model was implemented

--- product/models.py
+++ product/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-from core.models import TimeStampMixin,LogicalDeleteMixin
+from core.models import TimeStampMixin, LogicalDeleteMixin
+
 
 # Create your models here.
-class Product(TimeStampMixin,LogicalDeleteMixin):
+class Product(TimeStampMixin, LogicalDeleteMixin):
     name = models.CharField(max_length=250)
     price = models.PositiveIntegerField()
     title = models.TextField(max_length=250)
-    category = models.ForeignKey('Category',on_delete=models.PROTECT,related_name='product_category')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='product_category')
+
+
+class Category(TimeStampMixin, LogicalDeleteMixin):
+    name = models.CharField(max_length=250)
+
