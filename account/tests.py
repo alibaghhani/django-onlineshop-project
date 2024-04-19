b9154a46ca021c64cb061b01071b46883ed2cf2a
alibaghani <baghaniali2006@gmail.com>
2024-04-19 11:58:35 +0330

    user  models teest was implemented

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from account.models import User  # Replace 'your_app_name' with the actual name of your Django app
 
-# Create your tests here.
+class UserModelTestCase(TestCase):
+
+    def test_valid_email(self):
+        user = User.objects.create(
+            first_name='John',
+            last_name='Doe',
+            email='johndoe@gmail.com'
+        )
+
+        try:
+            user.full_clean()
+        except ValidationError as e:
+            self.fail(f"ValidationError raised: {e.message}")
+
+    def test_invalid_email(self):
+        with self.assertRaises(ValidationError) as context:
+            user = User.objects.create(
+                first_name='John',
+                last_name='Doe',
+                email='baghaniali2006@gmail.com'
+            )
+            user.full_clean()
+
+        self.assertEqual(
+            context.exception.messages[0],
+            'لطفا یک ایمیل درست وارد کنید'
+        )
