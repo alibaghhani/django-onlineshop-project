964b16b5bb1b9645048039e64e1b8ea1d6a1c662
alibaghani <baghaniali2006@gmail.com>
2024-04-19 04:58:36 +0330

    register the models in admin pannel

--- account/admin.py
+++ account/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import User,Address
 from product.models import Image,Category,Product
+from order.models import OrderItem,Order
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Address)
 admin.site.register(Image)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
