3f3c63c901195a79ca7f1e8bf083acbecce4cfd0
alibaghani <baghaniali2006@gmail.com>
2024-04-19 03:34:18 +0330

    order model was implemented

--- order/models.py
+++ order/models.py
@@ -1,18 +1,45 @@
 from django.db import models
-from core.models import TimeStampMixin,LogicalDeleteMixin
-from product.models import Product
+from core.models import TimeStampMixin, LogicalDeleteMixin
+from product.models import Product, Discount
 from account.models import User
-# Create your models here.
-class OrderItem(TimeStampMixin,LogicalDeleteMixin):
-    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='product-order')
-    quantity = models.PositiveIntegerField()
-    price = models.FloatField()
-    order = models.ForeignKey('Order',on_delete=models.CASCADE)
 
-    def get_total_price(self):
+
+# Create your models here.
+class OrderItem(TimeStampMixin, LogicalDeleteMixin):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_order')
+    quantity = models.PositiveIntegerField()
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='order_item')
+
+    @property
+    def price(self):
+        return self.product.price
+
+    @property
+    def total_price(self):
         return self.price * self.quantity
+
+    @property
+    def discount(self):
+        return Discount.objects.filter(product=self.product)
+
+
+
+    def discounted_price(self):
+        # return *[self.total_price()-(self.quantity*Discount.objects.filter(product=self.product)) if Discount.objects.filter(product=self.product)]
+        if self.discount:
+            return self.total_price - (self.quantity * self.discount)
+        else:
+            return None
 
     # def __str__(self):
     #     return f"{self.order}"
 
+
+class Order(TimeStampMixin, LogicalDeleteMixin):
+    # DISCOUNT_CHOICES = (
+    #     ('percentage', '%'),
+    #     ('cash', '$')
+    # )
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customer_order')
+    # discount = models.CharField(choices=DISCOUNT_CHOICES, max_length=250, null=True, blank=True)
 
