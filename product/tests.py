f538d01aa42a135b076e173518547b347389e015
alibaghani <baghaniali2006@gmail.com>
2024-04-19 05:11:51 +0330

    product model testcase

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Product, Category
 
-# Create your tests here.
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Electronics')
+
+        self.product = Product.objects.create(
+            name='Laptop',
+            price=1000,
+            title='High-Performance Laptop',
+            category=self.category
+        )
+
+    def test_product_str_method(self):
+        expected_str = f"{self.product.category}----{self.product.name}"
+        self.assertEqual(str(self.product), expected_str)
+
+
