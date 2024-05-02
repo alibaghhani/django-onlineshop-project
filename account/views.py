eb58cefa19fbcd98106e96354fad89447cec4b66
alibaghani <baghaniali2006@gmail.com>
2024-05-02 21:47:31 +0330

    some fixes in verify_email view

--- account/views.py
+++ account/views.py
@@ -1,7 +1,11 @@
-from random import randint
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+# from random import randint
+#
+# from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.urls import reverse_lazy
+# from django.utils.decorators import method_decorator
+# from django.views.decorators.csrf import csrf_exempt
+# from django.views.generic import CreateView
 from random import randint
 from django.contrib import messages
 from django.contrib.auth.views import LoginView
@@ -15,48 +19,89 @@ from sqlite3 import IntegrityError
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from account.models import User
+
 r = Redis(host='localhost', port=6379, decode_responses=True)
 from config import settings
 from .models import User
 
+#
+# class SignupView(CreateView):
+#     model = User
+#     template_name = 'signup.html'
+#     fields = ['username', 'password', 'email']
+#
+#     def form_valid(self, form):
+#         print('njnvknjknjkjnkjnjvrntinjvrtnjrijnbirjntbirubnir')
+#         print(self.request.POST)
+#         username = form.cleaned_data['username']
+#         password = form.cleaned_data['password']
+#         email = form.cleaned_data['email']
+#
+#         if User.objects.filter(email=email).exists():
+#             return self.form_invalid(form)
+#
+#         try:
+#             r.set(name=username, value=password)
+#             user = form.save(commit=False)
+#             user.is_active = False
+#             user.save()
+#
+#             otp_code = ''.join([str(randint(0, 9)) for _ in range(6)])
+#             r.set(name=f"{user.id}", value=f"{otp_code}")
+#             print(otp_code)
+#
+#             subject = 'Verification code'
+#             message = f'Your verification code is: {otp_code}'
+#             email_from = settings.EMAIL_HOST_USER
+#             recipient_list = [user.email, ]
+#             send_mail(subject, message, email_from, recipient_list)
+#             return super().form_valid(form)
+#         except IntegrityError:
+#             return self.form_invalid(form)
+#
+#     @property
+#     def success_url(self):
+#         return reverse_lazy('verify_email', kwargs={'email': self.object.email})
 
-class SignupView(CreateView):
-    model = User
+
+class SignupView(View):
     template_name = 'signup.html'
-    fields = ['username', 'password', 'email']
 
-    def form_valid(self, form):
-        print('njnvknjknjkjnkjnjvrntinjvrtnjrijnbirjntbirubnir')
-        print(self.request.POST)
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        email = form.cleaned_data['email']
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        if not all([username, password, email]):
+            return messages.error(request,"please fill al blanks")
 
         if User.objects.filter(email=email).exists():
-            return self.form_invalid(form)
+            return messages.error(request,'this email already exist')
+
+        if User.objects.filter(username=username).exists():
+            return messages.error(request,'this username already exist')
 
         try:
             r.set(name=username, value=password)
-            user = form.save(commit=False)
+            user = User.objects.create_user(username=username, email=email, password=password)
             user.is_active = False
             user.save()
 
             otp_code = ''.join([str(randint(0, 9)) for _ in range(6)])
-            r.set(name=f"{user.id}", value=f"{otp_code}")
+            r.set(name=str(user.id), value=otp_code)
             print(otp_code)
 
             subject = 'Verification code'
             message = f'Your verification code is: {otp_code}'
             email_from = settings.EMAIL_HOST_USER
-            recipient_list = [user.email, ]
+            recipient_list = [email]
             send_mail(subject, message, email_from, recipient_list)
-            return super().form_valid(form)
+            return redirect(reverse('verify_email',kwargs={'email':email}))
         except IntegrityError:
-            return self.form_invalid(form)
-
-    @property
-    def success_url(self):
-        return reverse_lazy('verify_email', kwargs={'email': self.object.email})
+            return messages.error(request,'An error occurred')
 
 
 def verify_email(request, email):
@@ -70,7 +115,7 @@ def verify_email(request, email):
             password = r.get(user.username)
             print(user.username, password)
             login(request, user)
-            return redirect('signup')
+            return redirect('register')
         else:
             return render(request, 'verify_email.html', {'error_message': 'invalid otp code.'})
     else:
