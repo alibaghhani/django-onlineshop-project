9eb2abd6e6e6d7ea9e60ac86bdeb40576c984583
alibaghani <baghaniali2006@gmail.com>
2024-05-03 11:26:56 +0330

    product detai view was commited

--- product/views.py
+++ product/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product,Image
 
-# Create your views here.
+
+
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'home.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = context['products']
+        product_images = {}
+        for product in products:
+            product_images[product] = Image.objects.filter(product=product)
+        context['product_images'] = product_images
+        return context
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'product_detail.html'
+
