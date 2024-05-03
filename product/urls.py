24930f08b355b39967dbddaf7af1fb942346ff88
alibaghani <baghaniali2006@gmail.com>
2024-05-03 14:41:39 +0330

    product app urls was comiited

--- product/urls.py
+++ product/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from config import settings
-from .views import ProductsListView,ProductDetailView
+from .views import ProductsListView,ProductDetailView,CategoryTreeView
 
 urlpatterns = [
    path('',ProductsListView.as_view(),name='products'),
-   path('product_detail/<str:pk>',ProductDetailView.as_view(),name="product_detail")
+   path('product_detail/<str:pk>',ProductDetailView.as_view(),name="product_detail"),
+   path('category_tree/', CategoryTreeView.as_view(), name='category_tree'),
+
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
