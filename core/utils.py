02ac691de0dd6b0eb2fa62bc80a325d9a3e8d9a2
alibaghani <baghaniali2006@gmail.com>
2024-05-03 14:38:47 +0330

    maker function was removed

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,13 @@
 from uuid import uuid4
 
 
-def maker(root: str, instance, filename: str, keys: list | None = None):
-    filename, extension = filename.split(".")
+# def maker(root: str, instance, filename: str, keys: list | None = None):
+#     filename, extension = filename.split(".")
+#
+#     sub_directories = [getattr(instance, field) for field in keys or []]
+#     return f"{root}/{'/'.join(sub_directories)}/{uuid4()}.{extension}"
 
-    sub_directories = [getattr(instance, field) for field in keys or []]
-    return f"{root}/{'/'.join(sub_directories)}/{uuid4()}.{extension}"
+
+# def maker(root: str, instance, filename: str, keys: list | None = None):
+#     filename, extension = filename.split(".")
+#     return f"{root}/{uuid4()}.{extension}"
