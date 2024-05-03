075fee9d4dbd92fcfaa27c9018a5e1cf7fbdd9b4
alibaghani <baghaniali2006@gmail.com>
2024-05-03 06:18:50 +0330

    change password verification view was implemented

--- account/views.py
+++ account/views.py
@@ -110,6 +110,28 @@ class ChangePassword(UpdateView):
     success_url = reverse_lazy('products')
 
 
+class VerifyEmailForChangePassword(View):
+    def get(self, request):
+        return render(request, 'login_with_email.html')
+
+    def post(self, request):
+        email = request.POST.get('email', False)
+
+        try:
+            print('nenvekjrnfilwjkervnekjrnfveikjrnvekjdrnfd ')
+            user = User.objects.get(email=email)
+            otp_code = ''.join([str(randint(0, 9)) for _ in range(6)])
+            print(otp_code)
+            r.set(name=f"{user.id}", value=f"{otp_code}")
+            subject = 'Verification code'
+            message = f'Your verification code is: {otp_code}'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [email, ]
+            send_mail(subject, message, email_from, recipient_list)
+            return redirect(reverse('verify_email', kwargs={'email': email}))
+        except ObjectDoesNotExist:
+            messages.error(request, "This email does not exist. You need to create an account first!")
+            return render(request, 'login_with_email.html')
 class SignInWithEmail(View):
     def get(self, request):
         return render(request, 'login_with_email.html')
