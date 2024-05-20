b0699bb41ae21938b3f92357b37c981d27a68fcc
alibaghani <baghaniali2006@gmail.com>
2024-05-20 22:53:14 +0330

    urls for category and subcategory view was implemented

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls.static import static
 from django.urls import path
 
-from config import settings
-from .views import ProductsListView,ProductDetailView,CategoryTreeView
+from django.conf import settings
+from .views import ProductsListView,ProductDetailView,CategoryTreeView,SubcategoryAndProducts
 
 urlpatterns = [
    path('',ProductsListView.as_view(),name='products'),
    path('product_detail/<str:pk>',ProductDetailView.as_view(),name="product_detail"),
    path('category_tree/', CategoryTreeView.as_view(), name='category_tree'),
+   path('product_category/<int:id>/', SubcategoryAndProducts.as_view(), name='category_product'),
 
 ]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
