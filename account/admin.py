b5b34192663066240b0a1bf1ea6f9d4481881ce0
alibaghani <baghaniali2006@gmail.com>
2024-05-20 20:18:41 +0330

    admin view implemented

--- account/admin.py
+++ account/admin.py
@@ -3,7 +3,6 @@ from .models import User, Address, UserProfile
 from product.models import Image,Category,Product,Discount,DiscountCode
 from order.models import OrderItem,Order
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Address)
 admin.site.register(Image)
 admin.site.register(Category)
@@ -13,3 +12,11 @@ admin.site.register(OrderItem)
 admin.site.register(Discount)
 admin.site.register(DiscountCode)
 admin.site.register(UserProfile)
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+admin.site.register(User, CustomUserAdmin)
