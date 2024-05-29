fd5404597bdb6e03c99954eaa328b62ee10e49c8
alibaghani <baghaniali2006@gmail.com>
2024-05-29 23:55:56 +0330

    cart class for adding stuff to cart using sessions was created

--- order/cart.py
+++ order/cart.py
@@ -2,7 +2,7 @@ from config.settings import ORDER_SESSION_ID
 from product.models import Product
 
 
-class Order:
+class Cart:
     def __init__(self, request):
         self.session = request.session
         order = self.session.get(ORDER_SESSION_ID)
@@ -31,6 +31,44 @@ class Order:
                 value['total_price'] = int(value['price']) * value['quantity']
                 yield value
 
+        # ids = self.order.keys()
+        # products = Product.objects.filter(id__in=ids)
+        # order = self.order.copy()
+        # for product in products:
+        #     if str(product.id) in order:
+        #         order[str(product.id)]['product'] = product
+        # for value in order.values():
+        #     if 'product' in value and 'quantity' in value:
+        #         product_price = value['product'].price if 'price' in value['product'] else 0
+        #         value['total_price'] = int(product_price) * value['quantity']
+        #         yield value
+        #
+        # ids = self.order.keys()
+        # products = Product.objects.filter(id__in=ids)
+        # order = self.order.copy()
+        #
+        # for product in products:
+        #     product_dict = {
+        #         'id': product.id,
+        #         'name': product.name,
+        #         'price': product.price,
+        #     }
+        #
+        #     if str(product.id) in order:
+        #         order[str(product.id)]['product'] = product_dict
+        #
+        # for value in order.values():
+        #     if 'product' in value and 'quantity' in value:
+        #         product_price = value['product']['price'] if 'price' in value['product'] else 0
+        #         value['total_price'] = int(product_price) * value['quantity']
+        #         yield value
+
+    # def add(self, product, quantity):
+    #     product_id = str(product.id)
+    #     if product_id not in self.order:
+    #         self.order[product_id] = {'quantity': 0, 'price': str(product.price)}
+    #     self.order[product_id]['quantity'] += quantity
+    #     self.save_session() asli
     def add(self, product, quantity):
         product_id = str(product.id)
         if product_id not in self.order:
@@ -38,10 +76,17 @@ class Order:
         self.order[product_id]['quantity'] += quantity
         self.save_session()
 
+    # def add(self, product, quantity):
+    #     product_id = str(product.id)
+    #     if product_id not in self.order:
+    #         self.order[product_id] = {'quantity': 0, 'product': product}
+    #     self.order[product_id]['quantity'] += quantity
+    #     self.save_session()
+
     def __len__(self):
         return sum(value['quantity'] for value in self.order.values())
 
-    def remove(self,product):
+    def remove(self, product):
         product_id = str(product.id)
         if product_id in self.order:
             del self.order[product_id]
@@ -49,6 +94,8 @@ class Order:
 
     def save_session(self):
         self.session.modified = True
+
+
 
     def get_final_price(self):
         return sum(value['product'].price * value['quantity'] for value in self.order.values())
