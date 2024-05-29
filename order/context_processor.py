d9a18ab3cb1d369ba19db98dc9699491676c3638
alibaghani <baghaniali2006@gmail.com>
2024-05-30 00:21:10 +0330

    context processor for using user and cart class

--- order/context_processor.py
+++ order/context_processor.py
@@ -1,4 +1,8 @@
-from .order import Order
-
+from django.contrib.auth import get_user_model
+from .cart import Cart
+user_ = get_user_model
 def order(request):
-    return {'order': Order(request)}
+    return {'order': Cart(request)}
+
+def user(request):
+    return {'user':user_}
