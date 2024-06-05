418faaeb2e647e02c84f33bfe891378daa71446c
alibaghani <baghaniali2006@gmail.com>
2024-06-06 02:29:53 +0330

    an api url for choosing address was made

--- order/api_urls.py
+++ order/api_urls.py
@@ -1,9 +1,9 @@
-from order.api_views import CartApiView,ProductAPI,OrderCreateApiView
+from order.api_views import CartApiView,ProductAPI,OrderCreateApiView,AddressChooseAPIView
 from django.urls import path
 
 urlpatterns = [
     path('cart/', CartApiView.as_view(), name='cart'),
     path('products/', ProductAPI.as_view(), name='products_api'),
     path('order/', OrderCreateApiView.as_view(), name='orders'),
-
+    path('choose_address/', AddressChooseAPIView.as_view(), name='api_address_choose')
 ]
