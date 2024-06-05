2e30f8ac4d1d1b2def40e398aab8ead5b1200093
alibaghani <baghaniali2006@gmail.com>
2024-06-06 02:31:20 +0330

    new method was added to order serializer

--- order/serializers.py
+++ order/serializers.py
@@ -1,24 +1,32 @@
 from rest_framework import serializers
+from account.serializers import AddressSerializer
 from .models import OrderItem,Order
 from product.models import Product
+
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
 
-    def create(self, validated_data):
-
-        return OrderItem.objects.create(
-            product=validated_data['product'],
-            quantity=validated_data['quantity'],
-        )
 
 class OrderSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True)
+    order_items = serializers.SerializerMethodField()
+    address = serializers.SerializerMethodField()
+
     class Meta:
         model = Order
         fields = '__all__'
+
+    def get_order_items(self, obj):
+        orderitems = obj.order_items.all()
+        return OrderItemSerializer(instance=orderitems, many=True).data
+
+    def get_address(self,obj):
+        address_ = obj.address.all()
+        return AddressSerializer(instance=address_,many=True).data
+
 
 
 
