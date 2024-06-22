46aebabc28c0e6bdde7c88830cc5efad46cc50f0
alibaghani <baghaniali2006@gmail.com>
2024-06-22 17:35:40 +0330

    Add userorder url path for Userordersview

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import (SignupView, verify_email, SignInWithEmail, SignInWithUsernameAndPassword,
-                    LogoutUser, UserProfileView, ChaneProfileView, ResetPasswordView,
-                    ResetPasswordDoneView, UserPasswordResetConfirmView, UserPasswordResetCompleteView
-                    , AllUsersAddresses, AddAddressView, LoginChoice,DeleteAddressView,AdminPannel)
+
+from .views import (AddAddressView, AdminPannel, AllUsersAddresses,
+                    ChaneProfileView, DeleteAddressView, DeleteUserOrder,
+                    LoginChoice, LogoutUser, ResetPasswordDoneView,
+                    ResetPasswordView, SignInWithEmail,
+                    SignInWithUsernameAndPassword, SignupView,
+                    UserOrderDetailView, UserOrdersView,
+                    UserPasswordResetCompleteView,
+                    UserPasswordResetConfirmView, UserProfileView,
+                    verify_email)
 
 urlpatterns = [
     # routes related to user authentication---------
@@ -14,8 +20,6 @@ urlpatterns = [
     path('logout/', LogoutUser.as_view(), name='logout'),
     path('choice_login_way/', LoginChoice.as_view(), name="login_choice"),
 
-
-
     # routes related to change password-----------
 
     path('reset_pass/', ResetPasswordView.as_view(), name="reset_password"),
@@ -23,14 +27,11 @@ urlpatterns = [
     path('confirm/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('confirm/complete', UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
-
-
-
     # routes related to user profile-------------
 
     path('profile/<int:pk>/', UserProfileView.as_view(), name='profile'),
     path('change_personal_information/<int:pk>/', ChaneProfileView.as_view(), name="change_profile"),
-
+    path('userorder/', UserOrdersView.as_view(), name='user-orders'),
 
 
     # routes related to user's address-------------
@@ -38,7 +39,6 @@ urlpatterns = [
     path('user_address/', AllUsersAddresses.as_view(), name='all_addresses'),
     path('add_new_address/', AddAddressView.as_view(), name="add_address"),
     path('delete-address/<int:pk>/', DeleteAddressView.as_view(), name='delete_address'),
-
 
     path('AdminPannel/', AdminPannel.as_view(), name='Admin')
 
