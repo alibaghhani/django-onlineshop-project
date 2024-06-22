f7c602e6b61776c3c82fb4456dc205957cc2a9bb
alibaghani <baghaniali2006@gmail.com>
2024-06-22 17:51:46 +0330

    Implement swagger schema and Add swagger's url

--- config/urls.py
+++ config/urls.py
@@ -6,19 +6,18 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views. home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework import permissions
 
 # ---- swagger schema for swagger setup
 schema_view = get_schema_view(
@@ -41,9 +40,8 @@ urlpatterns = [
     path('apiorder/', include('order.api_urls')),
     path('api_authentication/', include('account.api_urls')),
 
-
 ]
 
-urlpatterns +=  [path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-                 path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+urlpatterns += [path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+                ]
