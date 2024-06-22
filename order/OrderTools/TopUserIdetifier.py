871e99f43ee1e916ee40bb6556fc277a5449b670
alibaghani <baghaniali2006@gmail.com>
2024-06-22 19:23:15 +0330

    Implement Brilliantusers subclass

--- order/OrderTools/TopUserIdetifier.py
+++ order/OrderTools/TopUserIdetifier.py
@@ -1,3 +1,4 @@
+import logging
 from abc import ABC, abstractmethod
 
 from django.core.mail import send_mail
@@ -7,7 +8,6 @@ from account.models import User, UserProfile
 from config import settings
 from order.models import OrderItem
 from product.models import DiscountCode
-import logging
 
 logging.basicConfig(level=logging.INFO, filename="order/logs/order_logs.log", filemode="a")
 
@@ -33,6 +33,61 @@ class TopUsersFactory(ABC):
     @abstractmethod
     def send_notification(self, user, token):
         pass
+
+
+class BrilliantUsers(TopUsersFactory):
+    def __init__(self):
+        logging.info("entered into BrilliantUsers class")
+
+    def generate_discount_token(self, user):
+        code = get_random_string(length=5)
+        discount = 70
+        user = user
+        DiscountCode.objects.create(
+            code=code,
+            discount=discount,
+            type_of_discount='percentage',
+            user_id=user.id
+        )
+        return code
+
+    def check_is_paid_orders(self, user):
+        logging.info('Checking paid orders for BrilliantUsers')
+        user_payments = {}
+        order_items = OrderItem.objects.filter(order__customer_id=user, order__is_paid=True)
+        logging.info(order_items)
+        for item in order_items:
+            if item.id not in user_payments:
+                user_payments[item.id] = item.price
+        return user_payments
+
+    def calculate_final_amount(self, payments: dict):
+        logging.info('Calculating final amount for BrilliantUsers')
+        return sum(payments.values())
+
+    def user_type(self, user, amount: int):
+        logging.info(f'Determining user type for BrilliantUsers: user={user}, amount={amount}')
+        logging.info(type(amount))
+        if amount >= 500000:
+            logging.info('entered into if statement')
+            profile = UserProfile.objects.get(user_id=user)
+            logging.info(f'user type: {profile.type}')
+            profile.type = 'Brilliant'
+            profile.save()
+            logging.info(f'New user type: {profile.type}')
+            self.generate_discount_token(user)
+            return profile.type
+
+    def send_notification(self, user: User, token: str):
+        logging.info(f'Sending notification to {user.username} (BrilliantUsers)')
+        subject = f'congratulations {user.username}'
+        message = (f"you have reached 500000 order amount "
+                   f"here's your discount coupon {token} "
+                   "you are now a Brilliant user")
+
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [user.email]
+        send_mail(subject, message, email_from, recipient_list)
 
 
 
